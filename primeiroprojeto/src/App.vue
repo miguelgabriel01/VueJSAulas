84fc65223dfc251095be4567097e353a1d004322
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-19 16:10:28 -0300

    Envio de projetos iniciais com exemplos de criação de componentes e uso de vue em arquivos html

---
+++ primeiroprojeto/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <ButtonVue></ButtonVue>
+    <ButtonString></ButtonString>
+  </div>
+</template>
+
+<script>
+  import  ButtonVue from './ButtonVue.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ButtonVue,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ primeiroprojeto/src/ButtonVue.vue
@@ -0,0 +1,23 @@
+<template>
+ <button @click="click">{{count}} || times</button>
+</template>
+
+<script>
+export default {
+  name: "ButtonVue",
+  data(){
+    return {
+      count: 0,
+    };
+  },
+  methods:{
+    click(){
+      this.count++;
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+
+</style>
