97785758905f5ed9dd564ac6fdf7ade3aa9d28d6
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-19 17:29:31 -0300

    Exemplo do uso do computed que usa o cach para armazenar o estado de um componente e so altera depois que ele e  modificado

---
+++ metodosepropiedadescomputadas/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <button @click="Increment">Click</button>
+    {{ count }}
+    <small>pimeira vez</small><br>
+    {{fullName}}<br>
+    <small>segunda vez</small><br>
+    {{fullName}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{
+      count: 0,
+      nome: 'Miguel',
+      sobrenome: 'Gabriel'
+    }
+  },
+  computed:{
+   fullName(){
+     return Math.random();
+     //return this.nome + " " + this.sobrenome;
+   }
+  },
+  methods:{
+   Increment(){
+     this.count++
+   }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
