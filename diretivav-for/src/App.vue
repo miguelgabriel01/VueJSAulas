4059151405e8ee938013069862003a18e56fecdc
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-20 15:52:57 -0300

    Exemplo 1 de uso do v-for

---
+++ diretivav-for/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>Diretivas v-for</h1>
+    <ul>
+      <li v-for="(item,index) in todos" :key="index">
+        {{item}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data(){
+    return{
+      todos:['item 1','item 2','item 3'],
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
