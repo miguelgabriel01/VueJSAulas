b5b7b4896111203f5e6e9dec402f8b706a6e3cb1
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-20 14:11:27 -0300

    Exemplo basico de ciclo de vida de um componente

---
+++ ciclodevidadocomponente/src/Title.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Hello world</h1>
+</template>
+
+<script>
+export default {
+  name:'Title',
+  data(){
+    return{
+      hello:'world new day',
+    }
+  },
+  beforeCreate(){
+   console.log("BeforeCreate");
+   console.log(this.hello);
+  },
+  created() {
+    console.log("created");
+       console.log(this.hello);
+  },
+}
+</script>
+
+<style lang="scss">
+
+</style>
