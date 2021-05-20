d3a13371211db2c5166e004d8a7e7071afc25abd
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-20 17:03:18 -0300

    Exemplo de v-on passando expessao,metodo e metodo com parametro

---
+++ diretivasvoneventosemodificadores/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <h1>Diretivas V-on || Eventos || Modificadores</h1>
+    <button @click="count1++"> Click 1 com expressão || {{count1}}</button><br><br>
+    <button @click="increment"> Click 2 com metodo || {{count2}}</button><br><br>
+    <button @click="increment2(2)"> Click 3 com metodos e parametro passada || {{count3}}</button>
+    <listItem/>
+  </div>
+</template>
+
+<script> 
+import ListItem from './ListItem.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ListItem,
+  },
+  data(){
+    return{
+     count1:0,
+     count2:0,
+     count3:0,
+    }
+  },
+  methods:{
+    increment(){
+     this.count2++;
+    },
+    increment2(quantidade){
+      this.count3+=quantidade;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
