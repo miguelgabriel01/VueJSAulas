04055869f2b8ab8497e4ac6d8c81a54637188f1c
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2021-05-20 14:46:05 -0300

    Exemplo do uso do v-dind para alterar atributos do html

--- propiedadesediretivas/src/App.vue
+++ propiedadesediretivas/src/App.vue
@@ -3,6 +3,10 @@
     <h1 v-if="state === 'loading'">Loading..</h1>
     <h2 v-else-if="state === 'error'">Erro!!!</h2>
     <h3 v-else>Dados Atualizados com sucesso!</h3>
+
+    <p>Uso do V-bind</p>
+    <a v-bind:href="urlGoogle" :style="styleText">Google</a><br>
+    <a :href="urlYoutube" :style="styleText">Youtube</a>
   </div>
 </template>
 
@@ -14,7 +18,10 @@ export default {
   },
   data(){
     return{
-     state:'loading'
+     state:'loading',
+     urlGoogle:'www.google.com',
+     urlYoutube:'www.youtube.com',
+     styleText:{color:'green',textDecoration:'none',fontSize:'20px'},
     }
   },
 }
